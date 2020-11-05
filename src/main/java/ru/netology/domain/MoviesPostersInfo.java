72d9484b28ff313d89d6605b300d9e1d779140de
Ekaterina Popova <katerina.shavonsky@yandex.ru>
2020-11-05 18:53:38 +0300

    Branch Manager

---
+++ src/main/java/ru/netology/domain/MoviesPostersInfo.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+
+public class MoviesPostersInfo {
+    int id;
+    String movieName;
+    int moviePicture;
+    String movieGenre;
+
+    public
+    MoviesPostersInfo(int id, String movieName, int moviePicture, String movieGenre) {
+        this.id = id;
+        this.movieName = movieName;
+        this.moviePicture = moviePicture;
+        this.movieGenre = movieGenre;
+    }
+}
